f1c417b619eca42a5de3e73b35f55237fee08d1e
jose <a19josgartor@inspedralbes.cat>
2021-03-01 15:10:51 +0100

    Commit realizado por jose

--- src/projecteGrup3/HelloWorld.java
+++ src/projecteGrup3/HelloWorld.java
@@ -1,8 +1,11 @@
 package projecteGrup3;
 
 public class HelloWorld {
+	
+	//Actualizado por jose
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("Hello World by:Jose");
 	}
 }
